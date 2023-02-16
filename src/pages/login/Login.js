f6953fb3654e4c8c9e29317ff37c6af90f54dbfe
abhsiksah <killuawpak@gmail.com>
2023-02-16 14:48:55 +0530

    final Glogin changes i hope

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -63,6 +63,7 @@ export default function Login() {
                   {
                     email: decoded.email,
                     password: "google_Auth",
+                    username: decoded.name,
                   },
                   dispatch
                 );
